3b55653fae42a88e8e3e7a9744cd2e43c250ed62
congnc <95849309+conggaro@users.noreply.github.com>
2025-04-23 15:53:38 +0700

    Update SqlServerToExcel.ps1

--- SqlServerToExcel.ps1
+++ SqlServerToExcel.ps1
@@ -1,5 +1,5 @@
 # Kết nối và xuất dữ liệu
-$connectionString = "Password=tvc@123;User ID=sa;Initial Catalog=VANESA_LIVE;Data Source=192.168.60.22\SQLSERVER2022;TrustServerCertificate=True"
+$connectionString = "Your connection string"
 $query = "select * from HU_CONTRACT_TYPE"
 $Data = Invoke-Sqlcmd -ConnectionString $connectionString -Query $query
 $Data | Export-Excel -Path "C:\Users\CongPC\Music\outputfile.xlsx"
